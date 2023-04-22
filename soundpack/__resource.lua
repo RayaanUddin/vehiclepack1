f00c18fecfe019b8ba0fca4d9b854c9f3fbd6499
RayaanUddin <91953921+RayaanUddin@users.noreply.github.com>
2023-04-22 16:45:54 +0100

    sound update

---
+++ soundpack/__resource.lua
@@ -0,0 +1,90 @@
+fx_version 'adamant'
+game 'gta5'
+
+lua54 'on'
+
+server_scripts {
+	"server.lua"
+}
+client_scripts {
+	"client.lua",
+}
+
+files {
+	"audioconfig/*.dat151.rel",
+	"audioconfig/*.dat54.rel",
+	"audioconfig/*.dat10.rel",
+	"sfx/**/*.awc"
+}
+
+-- Nissan:GTR35
+data_file 'AUDIO_GAMEDATA' 'audioconfig/vr38dettv6_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/vr38dettv6_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_vr38dettv6'
+
+-- Nissan:R34
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/bnr34ffeng_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/bnr34ffeng_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/bnr34ffeng_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_bnr34ffeng'
+
+-- BMW:M4
+data_file "AUDIO_SYNTHDATA" "audioconfig/lg18bmwm4_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg18bmwm4_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg18bmwm4_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg18bmwm4"
+
+-- BMW:M3
+data_file 'AUDIO_GAMEDATA' 'audioconfig/s65b40_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/s65b40_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s65b40'
+
+-- BMW:M8
+data_file 'AUDIO_GAMEDATA' 'audioconfig/s63b44_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/s63b44_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s63b44'
+
+-- Farari:F40
+data_file "AUDIO_SYNTHDATA" "audioconfig/lgcy12ferf40_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lgcy12ferf40_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lgcy12ferf40_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lgcy12ferf40"
+
+-- Farari:F12
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta488f154_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta488f154_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta488f154'
+
+-- Volkswagen:Golf
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ea888_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ea888_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ea888'
+
+-- Dodge:Challenger
+data_file "AUDIO_SYNTHDATA" "audioconfig/lg81hcredeye_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg81hcredeye_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg81hcredeye_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg81hcredeye"
+
+-- Dodge:ChargerOld
+data_file 'AUDIO_GAMEDATA' 'audioconfig/cammedcharger_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/cammedcharger_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_cammedcharger'
+
+-- V8 Engine
+data_file 'AUDIO_GAMEDATA' 'audioconfig/aqls7raceswap_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aqls7raceswap_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aqls7raceswap'
+
+
+
+
+
+
+
+
+
+
+
+
+
